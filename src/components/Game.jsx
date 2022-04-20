303953c225cb4d486aacddfd82af932b466176eb
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-20 13:54:21 -0300

    all components

---
+++ src/components/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//css
+import "./Game"
+
+const Game = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Game
---
+++ src/components/GameOver.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//css
+import "./GameOver.css"
+
+const GameOver = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default GameOver
