796a74842692536308dfaf0fc7d1ffcbf10c50e8
miqueiasbelfort <kawasousa1554@gmail.com>
2022-04-20 13:43:21 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+//hooks
+import { useState, useCallback, useEffect } from 'react'
+//css
 import './App.css'
+//data
+import {wordsList} from "./data/words"
+
+import StartScreen from './components/StartScreen'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <StartScreen />
     </div>
   )
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//css
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Secret Word</h1>
+        <p>Clique no botão para jogar</p>
+        <button>Start Game</button>
+    </div>
+  )
+}
+
+export default StartScreen
